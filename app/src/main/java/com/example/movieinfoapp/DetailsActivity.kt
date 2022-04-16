8eb497ceb382bae5d606a0b58794ecadd5484d71
Masstran <15689794+Masstran@users.noreply.github.com>
2022-04-17 00:26:33 +0300

    Add share button functionality

--- app/src/main/java/com/example/movieinfoapp/DetailsActivity.kt
+++ app/src/main/java/com/example/movieinfoapp/DetailsActivity.kt
@@ -1,11 +1,24 @@
 package com.example.movieinfoapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        val shareButton = findViewById<Button>(R.id.share_button)
+        shareButton.setOnClickListener {
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Let's go watch movies!")
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(intent, null)
+            startActivity(shareIntent)
+        }
     }
 }
