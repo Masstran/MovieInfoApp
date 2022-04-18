a8bb343b2ebfa6a1af277dded875fe7d6a9eabef
Masstran <15689794+Masstran@users.noreply.github.com>
2022-04-18 21:57:29 +0300

    Add text to details activity and some view changes

--- app/src/main/java/com/example/movieinfoapp/DetailsActivity.kt
+++ app/src/main/java/com/example/movieinfoapp/DetailsActivity.kt
@@ -1,16 +1,28 @@
 package com.example.movieinfoapp
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import java.lang.Exception
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
         val shareButton = findViewById<Button>(R.id.share_button)
+
+        val movieId = intent.getStringExtra("movieId") ?: throw Exception("Null movieId came into DetailsActivity")
+
+        val movieData = MainActivity.movieData.getJSONObject(movieId)
+
+        val movieName = findViewById<TextView>(R.id.movie_name)
+        movieName.text = movieData.getString(MainActivity.movieNameKey)
+
+        val movieDescription = findViewById<TextView>(R.id.movie_description)
+        movieDescription.text = movieData.getString(MainActivity.movieDescriptionKey)
+
         shareButton.setOnClickListener {
             val intent = Intent().apply {
                 action = Intent.ACTION_SEND
--- app/src/main/java/com/example/movieinfoapp/MainActivity.kt
+++ app/src/main/java/com/example/movieinfoapp/MainActivity.kt
@@ -4,34 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
-import org.json.JSONArray
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setMovie(R.id.movie1, movie_data.getJSONObject(0))
-        setMovie(R.id.movie2, movie_data.getJSONObject(1))
-        setMovie(R.id.movie3, movie_data.getJSONObject(2))
+        for (movieId in movieData.keys()) {
+            when (movieId) {
+                "movie1" -> setMovie(R.id.movie1, movieId)
+                "movie2" -> setMovie(R.id.movie2, movieId)
+                "movie3" -> setMovie(R.id.movie3, movieId)
+            }
+        }
     }
 
-    private fun setMovie(id: Int, movie_data: JSONObject) {
+    private fun setMovie(id: Int, movieId: String) {
         val movie = findViewById<ConstraintLayout>(id)
-
+        val thisMovieData: JSONObject = movieData.getJSONObject(movieId)
         val movieName = movie.getViewById(R.id.movie_name)
         if (movieName !is TextView)
             throw Exception("Incorrect type of movieName")
-        movieName.text = movie_data.getString(movie_name_key)
+        movieName.text = thisMovieData.getString(movieNameKey)
 
         val movieDescription = movie.getViewById(R.id.movie_description)
         if (movieDescription !is TextView)
             throw Exception("Incorrect type of movieDescription")
-        movieDescription.text = movie_data.getString(movie_description_key)
+        movieDescription.text = thisMovieData.getString(movieDescriptionKey)
 
         val detailsButton = movie.getViewById(R.id.details_button)
         if (detailsButton !is Button)
@@ -39,31 +40,31 @@ class MainActivity : AppCompatActivity() {
 
         detailsButton.setOnClickListener {
             val intent = Intent(this,DetailsActivity::class.java)
+            intent.putExtra("movieId", movieId)
             startActivity(intent)
         }
     }
 
     companion object {
-        val movie_data : JSONArray = JSONArray("""
-            [{
-                "id": "movie1",
-                "movie_name": "Le Movie (2018)",
-                "movie_description": "This is a movie about something"
-             },
-             {
-                "id": "movie2",
-                "movie_name": "A movie about stuff (2016)",
-                "movie_description": "Definitely the best movie about stuff"
-             },
-             {
-                "id": "movie3",
-                "movie_name": "Doodely-doo (2020)",
-                "movie_description": "Doodles la doodles, lol"
-             }]
+        val movieData : JSONObject = JSONObject("""
+            {
+                "movie1": {
+                    "movie_name": "Le Movie (2018)",
+                    "movie_description": "This is a movie about something"
+                },
+                "movie2": {
+                    "movie_name": "A movie about stuff (2016)",
+                    "movie_description": "Definitely the best movie about stuff"
+                },
+                "movie3": {
+                    "movie_name": "Doodely-doo (2020)",
+                    "movie_description": "Doodles la doodles, lol"
+                }
+            }
         """.trimIndent())
 
-        val movie_name_key = "movie_name"
-        val movie_description_key = "movie_description"
+        const val movieNameKey = "movie_name"
+        const val movieDescriptionKey = "movie_description"
 
     }
 }
